8faac534e9c2ceccff936928569cc966ee6eb661
UsaSai99charanteja <saicharantejausac@gmail.com>
2024-02-07 11:56:00 -0600

    conflicts resolved

---
+++ professional_website/assets/js/index.js
@@ -0,0 +1,8 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://ucceijnbz4ery4jdg5qvlolb5e0ofzzl.lambda-url.us-east-1.on.aws/")
+    let data = await response.json()
+    counter.innerHTML = ` Views Report: ${data}`;
+}
+updateCounter();
+
