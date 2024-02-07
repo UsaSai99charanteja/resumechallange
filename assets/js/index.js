864b942d8b30a6847e10695cbf1a01b67d8a9353
UsaSai99charanteja <saicharantejausac@gmail.com>
2024-02-07 11:09:31 -0600

    resume challange

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,8 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://9c079lun24.execute-api.us-east-1.amazonaws.com/viewcount/")
+    let response = await fetch("https://ucceijnbz4ery4jdg5qvlolb5e0ofzzl.lambda-url.us-east-1.on.aws/")
     let data = await response.json()
     counter.innerHTML = ` Views Report: ${data}`;
 }
 updateCounter();
+
